277496bad3eb830450269e759752eb0cb14261c6
Joey Chen <jay.hatelove@gmail.com>
2021-01-06 14:48:46 +0800

    otp: use mockito create test double, instead of manual fake class

--- src/test/java/otp/AuthenticationServiceTest.java
+++ src/test/java/otp/AuthenticationServiceTest.java
@@ -1,19 +1,27 @@
 package otp;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 public class AuthenticationServiceTest {
 
     @Test
     public void is_valid_test() {
-        FakeProfile profile = new FakeProfile();
-        FakeToken token = new FakeToken();
+//        FakeProfile profile = new FakeProfile();
+        Profile profile = Mockito.mock(Profile.class);
+        when(profile.getPassword("joey")).thenReturn("91");
+
+//        FakeToken token = new FakeToken();
+        Token token = Mockito.mock(Token.class);
+        when(token.getRandom(anyString())).thenReturn("000000");
+
         AuthenticationService target = new AuthenticationService(profile, token);
 
         boolean actual = target.isValid("joey", "91000000");
-
         assertTrue(actual);
     }
 
