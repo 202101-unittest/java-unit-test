40ba3a027f30acd3e5fed6843695da0483e16375
Joey Chen <jay.hatelove@gmail.com>
2021-01-06 14:33:28 +0800

    holiday: pass test by extract and override getToday()

--- src/main/java/holiday/Holiday.java
+++ src/main/java/holiday/Holiday.java
@@ -4,10 +4,14 @@ import java.time.LocalDate;
 
 public class Holiday {
     public String sayHello() {
-        LocalDate today = LocalDate.now();
-        if (today.getMonthValue()==12&&today.getDayOfMonth()==25) {
+        LocalDate today = getToday();
+        if (today.getMonthValue() == 12 && today.getDayOfMonth() == 25) {
             return "Merry Xmas";
         }
         return "Today is not Xmas";
+    }
+
+    protected LocalDate getToday() {
+        return LocalDate.now();
     }
 }
--- src/test/java/holiday/HolidayTest.java
+++ src/test/java/holiday/HolidayTest.java
@@ -2,13 +2,28 @@ package holiday;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.Assert.assertEquals;
 
 public class HolidayTest {
     @Test
     public void today_is_xmas() {
-
-        Holiday holiday = new Holiday();
+        HolidayForTest holiday = new HolidayForTest();
+        holiday.setToday(LocalDate.of(2000, 12, 25));
         assertEquals("Merry Xmas", holiday.sayHello());
+    }
+
+    private class HolidayForTest extends Holiday {
+        private LocalDate today;
+
+        @Override
+        protected LocalDate getToday() {
+            return this.today;
+        }
+
+        public void setToday(LocalDate today) {
+            this.today = today;
+        }
     }
 }
