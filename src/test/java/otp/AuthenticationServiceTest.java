6f47e65de206056d74d94dafc74edf9ae311c72d
Joey Chen <jay.hatelove@gmail.com>
2021-01-06 14:51:55 +0800

    otp: add test "is_invalid"

--- src/test/java/otp/AuthenticationServiceTest.java
+++ src/test/java/otp/AuthenticationServiceTest.java
@@ -3,6 +3,7 @@ package otp;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -14,10 +15,22 @@ public class AuthenticationServiceTest {
     private AuthenticationService target = new AuthenticationService(profile, token);
 
     @Test
-    public void is_valid_test() {
+    public void is_valid() {
         givenPassword("joey", "91");
         givenToken("000000");
         shouldBeValid("joey", "91000000");
+    }
+
+    @Test
+    public void is_invalid() {
+        givenPassword("joey", "91");
+        givenToken("000000");
+        shouldBeInvalid("joey", "wrong password");
+    }
+
+    private void shouldBeInvalid(String account, String password) {
+        boolean actual = target.isValid(account, password);
+        assertFalse(actual);
     }
 
     private void shouldBeValid(String account, String password) {
